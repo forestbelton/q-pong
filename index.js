829582c904e3df7b962cfb45483bb37b8aca5f3e
Forest Belton <forest@homolo.gy>
2017-08-15 12:32:32 -0700

    Render game on every animation frame

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ const canvas: HTMLCanvasElement = document.getElementById('game')
 const context = canvas.getContext('2d')
 
 const game = new Grid(canvas.width, canvas.height)
-game.draw(context)
+
+function draw() {
+    game.draw(context)
+    window.requestAnimationFrame(draw)
+}
+
+window.requestAnimationFrame(draw)
