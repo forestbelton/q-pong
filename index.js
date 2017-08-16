4fe8f9736af67fb7dd58e88540b0f5c1b2bbffb6
Forest Belton <forest@homolo.gy>
2017-08-15 17:03:32 -0700

    Implement agent to follow the ball

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 // @flow
 import Grid from './lib/Grid'
+import FollowingAgent from './lib/agent/FollowingAgent'
+import Player from './lib/entity/Player'
 
 const canvas: HTMLCanvasElement = document.getElementById('game')
 const context = canvas.getContext('2d')
 
 const game = new Grid(canvas.width, canvas.height)
+const agents = [
+    new FollowingAgent(Player.ONE),
+    new FollowingAgent(Player.TWO)
+]
 
 function draw() {
+    agents.forEach(agent => {
+        const direction = agent.decideMove(game)
+        game.move(agent.player, direction)
+    })
+
     game.draw(context)
     window.requestAnimationFrame(draw)
 }
