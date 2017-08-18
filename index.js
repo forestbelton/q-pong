ffc9dc308e01294141b2c35ab8c5b993450b3e05
Forest Belton <forest@homolo.gy>
2017-08-17 22:37:15 -0700

    Implement agent with Q-learning

--- index.js
+++ index.js
@@ -1,25 +1,62 @@
+
 // @flow
+
 import Grid from './lib/Grid'
-import FollowingAgent from './lib/agent/FollowingAgent'
-import SedentaryAgent from './lib/agent/SedentaryAgent'
+import FollowingAgent from './lib/agent/agents/FollowingAgent'
+import QLearningAgent from './lib/agent/agents/q/QLearningAgent'
 import Player from './lib/entity/Player'
 
+const MOVES_PER_FRAME = 10
+
+let ticks = 0
+let sumTicks = 0
+
+let games = 0
+let wins = 0
+
+const summary = document.getElementById('summaryWins')
 const canvas: HTMLCanvasElement = document.getElementById('game')
+
+if (summary === null || canvas === null) {
+    throw new Error('Could not find necessary DOM elements')
+}
+
 const context = canvas.getContext('2d')
 
-const game = new Grid(canvas.width, canvas.height)
+let game = new Grid(canvas.width, canvas.height)
 const agents = [
     new FollowingAgent(Player.ONE),
-    new SedentaryAgent(Player.TWO)
+    new QLearningAgent(Player.TWO, canvas.width, canvas.height)
 ]
 
 function draw() {
-    game.update()
+    for (let i = 0; i < MOVES_PER_FRAME; ++i) {
+        ++ticks
 
-    agents.forEach(agent => {
-        const direction = agent.act(game)
-        game.move(agent.player, direction)
-    })
+        agents.forEach(agent => {
+            const direction = agent.act(game)
+            game.move(agent.player, direction)
+        })
+
+        game.update()
+
+        const winningPlayer = game.winningPlayer()
+        if (winningPlayer !== null) {
+            sumTicks += ticks
+            ticks = 0
+
+            ++games
+            if (winningPlayer === Player.TWO) {
+                ++wins
+            }
+
+            const percentageWon = (wins / games * 100).toFixed(2)
+            const averageTicks = Math.floor(sumTicks / games)
+            summary.textContent = `${wins}/${games} (${percentageWon}) - average game is ${averageTicks} ticks`
+
+            game = new Grid(canvas.width, canvas.height)
+        }
+    }
 
     game.draw(context)
     window.requestAnimationFrame(draw)
